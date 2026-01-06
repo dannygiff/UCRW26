ac4aaf635ecc8b939ea3b3bef06269fb08b50fb9
danny.giff <danny.giff@yahoo.com>
2026-01-05 20:16:04 -0800

    problem C

---
+++ CS141/HW1/C_stairs.cpp
@@ -0,0 +1,34 @@
+// Includes the entire std library for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // // Fast IO
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+
+    // int t = 1;
+    // cin >> t; // Keep this line if there are multiple test cases per input 
+    // while (t--) {
+    //     // Write the rest of the logic in here
+    // }
+
+    int n = 0;
+    cin >> n; // 5 <= n <= 10^4
+    vector<int> vals;
+    int dummy = 0;
+    for (int i=0; i<n; i++)
+    {
+        cin >> dummy;
+        vals.push_back(dummy);
+    }
+    int diff = vals[1] - vals[0];
+    for(int i=1; i<vals.size(); i++)
+    {
+        if ((vals[i] - vals[i-1]) != diff)
+        {
+            cout << i+1;
+            break;
+        }
+    }
+}
